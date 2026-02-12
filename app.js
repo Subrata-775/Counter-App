cf1f3e1e45860f27eb691abe9cc53dff712f1c9a
Subrata Jana <subratajana7735@gmail.com>
2026-02-12 13:46:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let pluseIcon = document.querySelector(".plus");
+let minusIcon = document.querySelector(".minus");
+let currvalue = document.querySelector(".value");
+
+pluseIcon.addEventListener("click", () => {
+    let currentvalue = parseInt(currvalue.textContent);
+    currentvalue++;
+    currvalue.textContent = currentvalue;
+});
+minusIcon.addEventListener("click", () => {
+    let currentvalue = parseInt(currvalue.textContent);
+    currentvalue--;
+    currvalue.textContent = currentvalue;
+});
